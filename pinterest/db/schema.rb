1cefab90dd01642a84b0f507c757a366b99bfa9e
Victoria Luc <victoriajeniluc@gmail.com>
2018-09-24 16:18:14 -0500

    migrate and delete a stylesheet

--- pinterest/db/schema.rb
+++ pinterest/db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180920051126) do
+ActiveRecord::Schema.define(version: 20180924211300) do
+
+  create_table "pins", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.datetime "created_on"
+    t.datetime "updated_on"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
