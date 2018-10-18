3dcc5b9e5d06ea57ad6b847e26289c545ed6ab25
Victoria Luc <victoriajeniluc@gmail.com>
2018-10-17 20:21:58 -0500

    error handle for if a picture is not added to the post

--- pinterest/app/models/pin.rb
+++ pinterest/app/models/pin.rb
@@ -2,4 +2,11 @@ class Pin < ApplicationRecord
 	has_one_attached :image
 	validates_presence_of :description, :title 
 	belongs_to :user
+	validate :image_validation 
+
+	def image_validation
+		if !image.attached?
+			errors[:base] << "NEEDS A PICTURE TO THIS!"
+		end 
+	end 
 end
